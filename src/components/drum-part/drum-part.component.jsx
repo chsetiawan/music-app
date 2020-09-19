e6019489ab82656d02bfb8586953a6b54a11727d
Christian <>
2020-09-19 01:04:23 -0700

    figured out how to play instruments from keycode

--- src/components/drum-part/drum-part.component.jsx
+++ src/components/drum-part/drum-part.component.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 
 import "./drum-part.component.scss";
 
-const DrumPart = ({ name, sound, num }) => {
+const DrumPart = ({ name, sound, num, keyCode }) => {
+  const escFunction = useCallback((event) => {
+    if(keyCode.includes(event.keyCode)) {
+      console.log(`you played: ${name}, letter pressed: ${String.fromCharCode(event.keyCode)}`)
+      playAudio();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const playAudio = () => {
     const audioEl = document.getElementsByClassName("audio-element")[num];
     audioEl.play();
--- src/components/drum/drum.component.jsx
+++ src/components/drum/drum.component.jsx
@@ -8,18 +8,31 @@ import DRUM_PARTS_DATA from '../../pages/drumpage/drum-parts.data';
 class Drum extends React.Component {
   constructor() {
     super();
+    
+    // this.escFunction = this.escFunction.bind(this);
 
     this.state = {
       drumParts: DRUM_PARTS_DATA
     }
   }
+  // escFunction(event){
+  //   if(event.keyCode === 27) {
+  //     console.log('yes');
+  //   }
+  // }
+  // componentDidMount(){
+  //   document.addEventListener("keydown", this.escFunction, false);
+  // }
+  // componentWillUnmount(){
+  //   document.removeEventListener("keydown", this.escFunction, false);
+  // }
 
   render() {
     return (
       <div className="drum-parts-container">
         {
-          this.state.drumParts.map(({ name, sound, num }) => (
-            <DrumPart name={name} sound={sound} num={num} />
+          this.state.drumParts.map(({ name, sound, num, keyCode }) => (
+            <DrumPart key={num} name={name} sound={sound} num={num} keyCode={keyCode} />
           ))
         }
       </div>
